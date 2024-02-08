ee45a0836057a826f16b508c30143a6875853420
941kjw <jawonkoo@naver.com>
2024-02-08 18:20:24 +0900

    [level 3] Title: 베스트앨범, Time: 7.50 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,44 +1,43 @@
 import java.util.*;
-import java.util.Map.Entry;
+import java.util.stream.*;
 
 class Solution {
+    
+    public class Music implements Comparable<Music>{
+        public int played;
+        public String genre;
+        public int index;
+        
+        public Music(int played,String genre,int index){
+            this.played = played;
+            this.genre = genre;
+            this.index = index;
+        }
+        
+        public String getGenre(){
+            return this.genre;
+        }
+        
+        @Override
+        public int compareTo(Music other){
+            if(this.played == other.played)
+                return this.index - other.index;
+            return other.played - this.played;
+            
+        }
+    }
   public int[] solution(String[] genres, int[] plays) {
-        int[] answer = {};
-        HashMap<String,Integer> map = new HashMap<>();
-        HashMap<Integer,Integer> map2 = new HashMap<>();
-
-
-        for(int i=0;i<genres.length;i++)    {
-            map.put(genres[i], map.getOrDefault(genres[i],0) + plays[i]);
-        }
-
-        ArrayList<String> lists =new ArrayList<>(map.keySet());
-
-        lists.sort(((o1,o2) -> map.get(o2) - map.get(o1)));
-
-        ArrayList<Integer> answerList = new ArrayList<>();
-
-        lists.forEach(li ->{
-            HashMap<Integer,Integer> playsMap = new HashMap<>();
-            for(int i=0;i<genres.length;i++){
-                if(li.equals(genres[i]))
-                    playsMap.put(i,plays[i]);
-            }
-
-            ArrayList<Integer> playsList = new ArrayList<>(playsMap.keySet());
-            playsList.sort((o1,o2) -> playsMap.get(o2) - playsMap.get(o1));
-
-            answerList.add(playsList.get(0));
-            if(playsList.size() != 1)
-                answerList.add(playsList.get(1));
-        });
-
-        answer = new int[answerList.size()];
-
-        for (int idx = 0; idx < answerList.size(); idx++) {
-            answer[idx] = answerList.get(idx);
-        }
-
-        return answer;
+               
+        return IntStream.range(0,genres.length)
+            .mapToObj(i->new Music(plays[i],genres[i],i))
+            .collect(Collectors.groupingBy(Music::getGenre))
+            .entrySet().stream()
+            .sorted((a,b) -> sum(b.getValue()) - sum(a.getValue()))
+            .flatMap(x -> x.getValue().stream().sorted().limit(2))
+            .mapToInt(x->x.index).toArray();
+    }
+    
+    private int sum(List<Music> values){
+        return values.stream().mapToInt(i -> i.played).sum();
     }
 }
