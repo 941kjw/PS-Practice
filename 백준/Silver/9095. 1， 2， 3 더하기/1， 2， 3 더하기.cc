564538a94c94df8e0fc89ea393cd84834de2cea4
941kjw <jawonkoo@naver.com>
2024-06-22 22:35:27 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -1,16 +1,13 @@
-#include <cstdio>
+#include <stdio.h>
 
-int dp[11] = { 0,1,2,4 };
+int dp[11] = { 0,1,2,4 },T,x;
 
 int main()
 {
-	int T,x;
-	scanf("%d", &T);
 	for (int i = 4; i < 11; i++) {
 		dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
 	}
-	while(T--){
-		scanf("%d", &x);
+	for(scanf("%d", &T); scanf("%d", &x) && T--;){
 		printf("%d\n", dp[x]);
 	}
 	return 0;
