3c487c31bb719f6dac822dcc6f5842dccc8305bc
941kjw <jawonkoo@naver.com>
2024-06-22 22:33:23 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -1,25 +1,17 @@
-#include <bits/stdc++.h>
-
-using namespace std;
+#include <cstdio>
 
 int dp[11] = { 0,1,2,4 };
 
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	int T;
-	cin >> T;
+	int T,x;
+	scanf("%d", &T);
 	for (int i = 4; i < 11; i++) {
 		dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
 	}
-
-	for (int i = 0; i < T; i++) {
-		int x;
-		cin >> x;
-		cout << dp[x] << '\n';
+	while(T--){
+		scanf("%d", &x);
+		printf("%d\n", dp[x]);
 	}
 	return 0;
 }
