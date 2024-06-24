9f8eda57c82f66d2860e0e560b0faf90efb0be00
941kjw <jawonkoo@naver.com>
2024-06-24 13:56:45 +0900

    [Gold V] Title: 평범한 배낭, Time: 40 ms, Memory: 42004 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -3,7 +3,6 @@
 using namespace std;
 
 vector<pair<int, int>> items;
-vector<vector<int>> b;
 int n, k;
 
 int main() {
@@ -12,13 +11,12 @@ int main() {
 	cout.tie(NULL);
 
 	cin >> n >> k;
-	b = vector<vector<int>>(n+1, vector<int>(k+1,0));
-	items.push_back({ 0,0 });
-
+	vector<pair<int, int>> items(n + 1);
+	vector<vector<int>> b (n+1, vector<int>(k+1,0));
 	for (int i = 1; i <= n; i++) {
 		int w, v;
 		cin >> w >> v;
-		items.push_back({ w,v });
+		items[i] = { w,v };
 	}
 
 	for (int i = 1; i <= n; i++) {
