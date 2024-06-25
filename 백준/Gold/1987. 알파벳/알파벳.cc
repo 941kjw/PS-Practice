9dab47893ea2a91b50167a1634c8f3e05f83f73d
941kjw <jawonkoo@naver.com>
2024-06-25 15:30:06 +0900

    [Gold IV] Title: 알파벳, Time: 20 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -5,24 +5,27 @@ using namespace std;
 int R,C;
 
 vector<vector<char>> items;
-vector<bool> visited = vector<bool>(26);
+vector<vector<int>> dp;
 int maxl;
 
 int dx[4] = { 1,-1,0,0 };
 int dy[4] = { 0,0,1,-1 };
 
-void DFS(int y, int x,int count) {
+void DFS(int y, int x,int count,int visited) {
+	visited |= (1 << items[y][x]);
+	if (dp[y][x] == visited)
+		return;
 	maxl = max(maxl, count);
+
+	dp[y][x] = visited;
+
 	for (int i = 0; i < 4; i++) {
 		int ny = y + dy[i];
 		int nx = x + dx[i];
 		if (nx < 0 || nx >= C || ny < 0 || ny >= R)
 			continue;
-		int charIndex = items[ny][nx] - 'A';
-		if (!visited[charIndex]) {
-			visited[charIndex] = true;
-			DFS(ny, nx, count + 1);
-			visited[charIndex] = false;
+		if (!(visited & (1 << items[ny][nx]))) {
+			DFS(ny, nx, count + 1,visited);
 		}
 	}
 
@@ -35,14 +38,14 @@ int main() {
 
 	cin >> R >> C;
 	items = vector<vector<char>>(R, vector<char>(C));
+	dp = vector<vector<int>>(R, vector<int>(C));
 	for (int i = 0; i < R; i++) {
 		for (int j = 0; j < C; j++) {
 			cin >> items[i][j];
 		}
 	}
 	maxl = 1;
-	visited[items[0][0] - 'A'] = true;
-	DFS(0, 0,1);
+	DFS(0, 0,1, 0);
 
 	cout << maxl;
 	return 0;
