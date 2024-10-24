d753a18979355dfea763369a7312abc7e2af64ad
941kjw <jawonkoo@naver.com>
2024-10-24 13:31:32 +0900

    [level 1] Title: 모의고사, Time: 0.68 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -4,23 +4,31 @@ class Solution {
     public int[] solution(int[] answers) {
         ArrayList<Integer> answer = new ArrayList<>();
         int[] one = {1,2,3,4,5};
+        int ol = one.length;
         int[] two = {2,1,2,3,2,4,2,5};
+        int tl = two.length;
         int[] three = {3,3,1,1,2,2,4,4,5,5};
-        int[] score = new int[3];
+        int hl = three.length;
+        int score1 = 0;
+        int score2 = 0;
+        int score3 = 0;
         for(int i =0; i<answers.length; i++){
-            if(one[i%one.length] == answers[i])
-                score[0]++;
-            if(two[i%two.length] == answers[i])
-                score[1]++;
-            if(three[i%three.length] == answers[i])
-                score[2]++;
+            if(one[i%ol] == answers[i])
+                score1++;
+            if(two[i%tl] == answers[i])
+                score2++;
+            if(three[i%hl] == answers[i])
+                score3++;
         }
-        int result = Math.max(score[0],Math.max(score[1],score[2]));
-        for(int i=0;i<3;i++){
-            if(result == score[i])
-                answer.add(i+1);
-        }        
+        int result = Math.max(score1,Math.max(score2,score3));
+        if(result == score1) answer.add(1);
+        if(result == score2) answer.add(2);
+        if(result == score3) answer.add(3);
+        int cnt = 0;
+        int[] r = new int[answer.size()];
+        for(int i : answer)
+            r[cnt++] = i;
         
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        return r;
     }
 }
