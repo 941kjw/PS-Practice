d33f7ec2b3ef0240a28e33a52603f93805f5c081
941kjw <jawonkoo@naver.com>
2024-11-11 12:35:07 +0900

    [Gold V] Title: MooTube (Silver), Time: 112 ms, Memory: 2288 KB -BaekjoonHub

---
+++ 백준/Gold/15591. MooTube （Silver）/MooTube （Silver）.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#define pii pair<int,int>
+using namespace std;
+
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N,Q;
+	cin >> N >> Q;
+	vector<pii> vs[5001];
+	for (int i = 0; i < N-1; i++) {
+		int p, q, r;
+		cin >> p >> q >> r;
+
+		vs[p].push_back({ q,r });
+		vs[q].push_back({ p,r });
+	}
+
+	queue<int> q;	
+	vector<int> visited;
+	for (int i = 0; i < Q; i++) {
+		visited = vector<int>(N + 1);
+		int k,v;
+		cin >> k>>v;
+		int c = 0;
+		q.push(v);
+		visited[v] = true;
+		while (!q.empty()) {
+			int cur = q.front();
+			q.pop();
+			for (int j = 0; j < vs[cur].size(); j++) {
+				if (vs[cur][j].second >= k && !visited[vs[cur][j].first]) {
+					q.push(vs[cur][j].first);
+					visited[vs[cur][j].first] = true;
+					c++;
+				}
+			}
+		}
+		cout << c << '\n';
+	}
+}
