021bcc5be488d5957afb1381167606489ae4a01c
941kjw <jawonkoo@naver.com>
2024-11-15 12:50:48 +0900

    [Silver I] Title: 스티커, Time: 84 ms, Memory: 5240 KB -BaekjoonHub

---
+++ 백준/Silver/9465. 스티커/스티커.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n,t;
+vector<vector<int>> board;
+vector<vector<int>> dp;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	
+	cin >> t;
+	while (t--) {
+
+		cin >> n;
+		board.assign(2, vector<int>(n + 1, 0));
+		dp.assign(2, vector<int>(n + 1, 0));
+		
+		for (int i = 0; i < 2; i++) {
+			for (int j = 1; j <= n; j++) {
+				cin >> board[i][j];
+			}
+		}
+		dp[0][0] = dp[1][0] = 0;
+		dp[0][1] = board[0][1];
+		dp[1][1] = board[1][1];
+		for (int i = 2; i <= n; i++) {
+			dp[0][i] = max(dp[1][i - 1], dp[1][i - 2]) + board[0][i];
+			dp[1][i] = max(dp[0][i - 1], dp[0][i - 2]) + board[1][i];
+		}
+
+		cout << max(dp[0][n], dp[1][n])<<"\n";
+	}
+}
