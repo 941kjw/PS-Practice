3d02df337a8fb1ec27b30a007e5fd8d5c44bdbfe
941kjw <jawonkoo@naver.com>
2025-02-26 17:42:19 +0900

    [Silver V] Title: 스네이크버드, Time: 72 ms, Memory: 12612 KB -BaekjoonHub

--- 백준/Silver/16435. 스네이크버드/스네이크버드.java
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.java
@@ -1,3 +1,4 @@
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.StreamTokenizer;
@@ -42,7 +43,7 @@ public class Main {
 	}
 
 	public static void main(String[] args) throws IOException {
-		InputStreamReader reader = new InputStreamReader(System.in);
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		StreamTokenizer tokenizer = new StreamTokenizer(reader);
 		init(tokenizer);
 
