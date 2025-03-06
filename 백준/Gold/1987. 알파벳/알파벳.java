57c0e1fb7504c80acb4a173d0cce90082ba6b180
941kjw <jawonkoo@naver.com>
2025-03-06 14:50:52 +0900

    [Gold IV] Title: 알파벳, Time: 904 ms, Memory: 12204 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -2,12 +2,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.StreamTokenizer;
-import java.util.HashSet;
-import java.util.Set;
 
 public class Main {
 
-	static char[][] board;
+	static int[][] board;
 	static int boardRow;
 	static int boardCol;
 	static int maxRouteLength;
@@ -18,44 +16,36 @@ public class Main {
 	static void init(StreamTokenizer tokenizer) throws IOException {
 		boardRow = read(tokenizer);
 		boardCol = read(tokenizer);
-		maxRouteLength = 1;
-		board = new char[boardRow][boardCol];
+		maxRouteLength = -1;
+		board = new int[boardRow][boardCol];
 
 		for (int row = 0; row < boardRow; row++) {
 			String line = readLine(tokenizer);
 			for (int col = 0; col < boardCol; col++) {
-				board[row][col] = line.charAt(col);
+				board[row][col] = line.charAt(col) - 'A';
 			}
 		}
 	}
 
-	static void findMaxRoute() {
-		dfs(new Pos(0, 0));
-	}
+	static void dfs(int row, int col, int count, int visited) {
 
-	static void dfs(Pos pos) {
-		pos.trace.add(board[pos.row][pos.col]);
-		int originRow = pos.row;
-		int originCol = pos.col;
+		if ((visited & (1 << board[row][col])) != 0) {
+			return;
+		}
 
-		maxRouteLength = Math.max(maxRouteLength, pos.trace.size());
+		visited |= (1 << board[row][col]);
+
+		maxRouteLength = Math.max(count, maxRouteLength);
 
 		for (int dir = 0; dir < 4; dir++) {
-			int nrow = pos.row + dy[dir];
-			int ncol = pos.col + dx[dir];
+			int nrow = row + dy[dir];
+			int ncol = col + dx[dir];
 
-			if (nrow < 0 || nrow >= boardRow || ncol < 0 || ncol >= boardCol || pos.trace.contains(board[nrow][ncol]))
+			if (nrow < 0 || nrow >= boardRow || ncol < 0 || ncol >= boardCol)
 				continue;
 
-			pos.trace.remove(board[nrow][ncol]);
-			pos.row = nrow;
-			pos.col = ncol;
-
-			dfs(pos);
-
-			pos.trace.remove(board[nrow][ncol]);
-			pos.row = originRow;
-			pos.col = originCol;
+			if ((visited & (1 << board[nrow][ncol])) == 0)
+				dfs(nrow, ncol, count + 1, visited);
 		}
 	}
 
@@ -64,7 +54,7 @@ public class Main {
 		StreamTokenizer tokenizer = new StreamTokenizer(reader);
 
 		init(tokenizer);
-		findMaxRoute();
+		dfs(0, 0, 1, 0);
 
 		System.out.println(maxRouteLength);
 	}
@@ -77,17 +67,6 @@ public class Main {
 	static String readLine(StreamTokenizer tokenizer) throws IOException {
 		tokenizer.nextToken();
 		return tokenizer.sval;
-	}
-
-	static class Pos {
-		int row, col;
-		Set <Character> trace;
-
-		public Pos(int row, int col) {
-			this.row = row;
-			this.col = col;
-			this.trace = new HashSet <>();
-		}
 	}
 
 }
