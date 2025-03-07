2f24aa44e77210d52aab0cd9dde9a3bb552ac49a
941kjw <jawonkoo@naver.com>
2025-03-07 11:34:18 +0900

    [Unrated] Title: [모의 SW 역량테스트] 미생물 격리, Time: 307 ms, Memory: 29,696 KB -BaekjoonHub

--- SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.java
+++ SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.java
@@ -5,176 +5,206 @@ import java.io.StreamTokenizer;
 
 public class Solution {
 
-    static int length;
-    static int targetTime;
-    static int groupCount;
+	static int length;
+	static int targetTime;
+	static int groupCount;
 
-    static MicroOrganism head;
-    static MicroOrganism tail;
-    static int microOrganismSum;
+	static MicroOrganism head;
+	static MicroOrganism tail;
 
-    static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {-1, 1, 0, 0};
+	static int[][] countMap;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        StreamTokenizer tokenizer = new StreamTokenizer(reader);
-        StringBuilder builder = new StringBuilder();
-        int testCount = read(tokenizer);
+	static int microOrganismSum;
 
-        for (int testNumber = 1; testNumber <= testCount; testNumber++) {
-            init(tokenizer);
+	static int[] dx = { 0, 0, -1, 1 };
+	static int[] dy = { -1, 1, 0, 0 };
 
-            simulate();
-            builder.append('#').append(testNumber).append(' ').append(microOrganismSum).append('\n');
-        }
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		StreamTokenizer tokenizer = new StreamTokenizer(reader);
+		StringBuilder builder = new StringBuilder();
+		int testCount = read(tokenizer);
 
-        System.out.println(builder);
-    }
+		for (int testNumber = 1; testNumber <= testCount; testNumber++) {
+			init(tokenizer);
 
-    static void init(StreamTokenizer tokenizer) throws IOException {
-        length = read(tokenizer);
-        targetTime = read(tokenizer);
-        groupCount = read(tokenizer);
-        head = null;
-        tail = null;
-        microOrganismSum = 0;
+			simulate();
+			builder.append('#').append(testNumber).append(' ').append(microOrganismSum).append('\n');
+		}
 
-        for (int idx = 0; idx < groupCount; ++idx) {
-            int row = read(tokenizer);
-            int col = read(tokenizer);
-            int amount = read(tokenizer);
-            int direction = read(tokenizer) - 1;
-            if (head == null) {
-                head = new MicroOrganism(row, col, amount, direction);
-                tail = head;
-            } else {
-                tail.attach(new MicroOrganism(row, col, amount, direction));
-                tail = tail.next;
-            }
-        }
-    }
+		System.out.println(builder);
+	}
 
-    static void simulate() {
-        for (int time = 0; time < targetTime; ++time) {
-            MicroOrganism current = head;
+	static void init(StreamTokenizer tokenizer) throws IOException {
+		length = read(tokenizer);
+		targetTime = read(tokenizer);
+		groupCount = read(tokenizer);
 
-            while (current != null) {
-                current.update();
-                current = current.next;
-            }
+		countMap = new int[length][length];
+		head = null;
+		tail = null;
+		microOrganismSum = 0;
 
-            current = head;
-            while (current != null) {
-                if (current.isDead()) {
-                    current = current.next;
-                    continue;
-                }
+		for (int idx = 0; idx < groupCount; ++idx) {
+			int row = read(tokenizer);
+			int col = read(tokenizer);
+			int amount = read(tokenizer);
+			int direction = read(tokenizer) - 1;
+			if (head == null) {
+				head = new MicroOrganism(row, col, amount, direction);
+				tail = head;
+			} else {
+				tail.attach(new MicroOrganism(row, col, amount, direction));
+				tail = tail.next;
+			}
+		}
+	}
 
-                int max = current.amount;
+	static void simulate() {
+		for (int time = 0; time < targetTime; ++time) {
+			updateAll();
 
-                MicroOrganism other = current.next;
+			checkCollision();
 
-                while (other != null) {
-                    if (other.isDead()) {
-                        other = other.next;
-                        continue;
-                    }
+			removeDead();
+		}
+		getSum();
+	}
 
-                    if (current.isCollided(other)) {
-                        current.amount += other.amount;
-                        if (max < other.amount) {
-                            current.direction = other.direction;
-                            max = other.amount;
-                        }
+	static void updateAll() {
+		MicroOrganism current = head;
 
-                        other.amount = 0;
-                    }
-                    other = other.next;
-                }
-                current = current.next;
-            }
+		while (current != null) {
+			current.update();
+			countMap[current.row][current.col]++;
+			current = current.next;
+		}
+	}
 
-            current = head;
-            while (current != null) {
-                MicroOrganism next = current.next;
-                if (current.isDead()) {
-                    current.removeSelf();
-                }
-                current = next;
-            }
-        }
+	static void removeDead() {
+		MicroOrganism current = head;
+		while (current != null) {
+			MicroOrganism next = current.next;
+			if (current.isDead()) {
+				current.removeSelf();
+			}
+			current = next;
+		}
+	}
 
-        MicroOrganism iterator = head;
-        while (iterator != null) {
-            microOrganismSum += iterator.amount;
-            iterator = iterator.next;
-        }
-    }
+	static void getSum() {
+		MicroOrganism iterator = head;
+		while (iterator != null) {
+			microOrganismSum += iterator.amount;
+			iterator = iterator.next;
+		}
+	}
 
-    static int read(StreamTokenizer tokenizer) throws IOException {
-        tokenizer.nextToken();
-        return (int) tokenizer.nval;
-    }
+	static void checkCollision() {
+		for (int row = 0; row < length; row++) {
+			for (int col = 0; col < length; col++) {
+				if (countMap[row][col] >= 2) {
+					handleCollision(row, col);
+				}
+				countMap[row][col] = 0;
+			}
+		}
+	}
 
-    static class MicroOrganism {
-        int row, col;
-        int amount;
-        int direction;
+	static void handleCollision(int row, int col) {
+		MicroOrganism current = head;
+		while (current != null) {
+			if (current.row == row && current.col == col) {
+				break;
+			}
+			current = current.next;
+		}
 
-        MicroOrganism before;
-        MicroOrganism next;
+		int max = current.amount;
 
-        public MicroOrganism(int row, int col, int amount, int direction) {
-            this.row = row;
-            this.col = col;
-            this.amount = amount;
-            this.direction = direction;
-            this.before = null;
-            this.next = null;
-        }
+		MicroOrganism other = current.next;
 
-        void attach(MicroOrganism next) {
-            this.next = next;
-            next.before = this;
-        }
+		while (other != null) {
+			if (other.row == row && other.col == col) {
+				current.amount += other.amount;
 
-        void update() {
-            if (isDead()) {
-                return;
-            }
+				if (max < other.amount) {
+					current.direction = other.direction;
+					max = other.amount;
+				}
+				other.amount = 0;
+			}
+			other = other.next;
+		}
+	}
 
-            this.row += dy[direction];
-            this.col += dx[direction];
+	static int read(StreamTokenizer tokenizer) throws IOException {
+		tokenizer.nextToken();
+		return (int) tokenizer.nval;
+	}
 
-            if (this.row == 0 || this.row == length - 1 || this.col == 0 || this.col == length - 1) {
-                this.direction ^= 1;
-                this.amount /= 2;
-            }
-        }
+	static class MicroOrganism {
+		int row, col;
+		int amount;
+		int direction;
 
-        boolean isCollided(MicroOrganism other) {
-            return (this.row == other.row) && (this.col == other.col);
-        }
+		MicroOrganism before;
+		MicroOrganism next;
 
-        boolean isDead() {
-            return this.amount == 0;
-        }
+		public MicroOrganism(int row, int col, int amount, int direction) {
+			this.row = row;
+			this.col = col;
+			this.amount = amount;
+			this.direction = direction;
+			this.before = null;
+			this.next = null;
+		}
 
-        void removeSelf() {
-            if (this != head) {
-                this.before.next = this.next;
-            } else {
-                this.next.before = null;
-                head = next;
-            }
+		void attach(MicroOrganism other) {
+			this.next = other;
+			other.before = this;
+		}
 
-            if (this != tail) {
-                this.next.before = this.before;
-            } else {
-                this.before.next = null;
-                tail = this.before;
-            }
-        }
-    }
+		void update() {
+			if (isDead()) {
+				return;
+			}
+
+			this.row += dy[direction];
+			this.col += dx[direction];
+
+			if (this.row == 0 || this.row == length - 1 || this.col == 0 || this.col == length - 1) {
+				this.direction ^= 1;
+				this.amount /= 2;
+			}
+		}
+
+		boolean isCollided(MicroOrganism other) {
+			return (this.row == other.row) && (this.col == other.col);
+		}
+
+		boolean isDead() {
+			return this.amount == 0;
+		}
+
+		int posHash() {
+			return this.row * 1000 + this.col;
+		}
+
+		void removeSelf() {
+			if (this != head) {
+				this.before.next = this.next;
+			} else {
+				this.next.before = null;
+				head = next;
+			}
+
+			if (this != tail) {
+				this.next.before = this.before;
+			} else {
+				this.before.next = null;
+				tail = this.before;
+			}
+		}
+	}
 }
