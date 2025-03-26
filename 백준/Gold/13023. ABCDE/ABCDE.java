866b8d41545c2c46459a9d9dbf50611f1e71191e
941kjw <jawonkoo@naver.com>
2025-03-26 16:01:22 +0900

    [Gold V] Title: ABCDE, Time: 184 ms, Memory: 18604 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -2,13 +2,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.StreamTokenizer;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
 
-	private static List<List<Integer>> relationList;
+	private static List<Integer>[] relationList;
 	private static boolean[] visited;
 
 	private static int manCount;
@@ -18,18 +17,18 @@ public class Main {
 		manCount = read(tokenizer);
 		relationCount = read(tokenizer);
 		visited = new boolean[manCount];
-		relationList = new ArrayList<>(manCount);
+		relationList = new List[manCount];
 
 		for (int idx = 0; idx < manCount; idx++) {
-			relationList.add(new LinkedList<>());
+			relationList[idx] = new LinkedList<>();
 		}
 
 		for (int idx = 0; idx < relationCount; idx++) {
 			int element1 = read(tokenizer);
 			int element2 = read(tokenizer);
 
-			relationList.get(element1).add(element2);
-			relationList.get(element2).add(element1);
+			relationList[element1].add(element2);
+			relationList[element2].add(element1);
 		}
 	}
 
@@ -53,20 +52,13 @@ public class Main {
 		}
 		System.out.println(0);
 	}
-
-	/**
-	 * 
-	 * 깊이 정보를 재사용하고 싶다!!
-	 * 진입했을 때, 2명이고, 이미 칸에 3명이면, 반환?
-	 * 
-	 * 
-	 */
+    
 	private static boolean dfs(int number, int depth) {
 		if (depth == 4)
 			return true;
 
 		boolean result = false;
-		for (Integer idx : relationList.get(number)) {
+		for (Integer idx : relationList[number]) {
 			if (!visited[idx]) {
 				visited[idx] = true;
 				if (dfs(idx, depth + 1)) {
