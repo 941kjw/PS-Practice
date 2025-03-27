528f3456a94b4200bb4ad0c6dbc716524f6dc7f7
941kjw <jawonkoo@naver.com>
2025-03-27 13:35:49 +0900

    [D4] Title: 최소 스패닝 트리, Time: 1,805 ms, Memory: 102,948 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -4,6 +4,18 @@ import java.io.InputStreamReader;
 import java.io.StreamTokenizer;
 import java.util.Arrays;
 
+/**
+ * 
+ * 단순한 크루스칼 알고리즘 문제
+ * 
+ * 
+ * 1. 입력받은 간선을 작은 순서대로 정렬한다.
+ * 2. 최상단 간선부터 선택하며, union-find를 통해 같은 그래프 상에 존재하는 정점이면 건너뛴다.
+ * 3. 정점 수 -1 만큼 선택하면 종료한다.
+ * 
+ *
+ */
+
 public class Solution {
 
 	private static int vertexCount;
@@ -53,13 +65,13 @@ public class Solution {
 		if (fromParent == toParent)
 			return false;
 
-		if (ranks[fromParent] > ranks[toParent]) {
-			parents[toParent] = fromParent;
-			return true;
-		}
-
-		if (ranks[fromParent] == ranks[toParent])
-			ranks[toParent]++;
+//		if (ranks[fromParent] > ranks[toParent]) {
+//			parents[toParent] = fromParent;
+//			return true;
+//		}
+//
+//		if (ranks[fromParent] == ranks[toParent])
+//			ranks[toParent]++;
 
 		parents[fromParent] = toParent;
 
