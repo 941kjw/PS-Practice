d65ed4f97556fe877795180a73802933258f4e92
941kjw <jawonkoo@naver.com>
2025-04-03 14:35:26 +0900

    [Unrated] Title: [모의 SW 역량테스트] 등산로 조성, Time: 78 ms, Memory: 25,088 KB -BaekjoonHub

--- SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
+++ SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
@@ -3,12 +3,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.StreamTokenizer;
 
+/**
+ * 
+ * DFS를 통해 경로를 탐색한다.
+ * BFS로 탐색하면, 어떤 경로의 이후 탐색해야할 경로가 이전 경로의 분기점에 의해 먼저 탐색되어 막혀버린다...
+ * 
+ * 1.최초 입력받을 때, 미리 가장 높은 봉우리의 위치를 저장해 둔다.
+ * 2.DFS 실행
+ * 		2-1. 일반적으로 이동하는 경로를 모두 탐색하고, 팔 수 있다면 파는 경로를 그 뒤에 탐색한다.
+ *		2-2. 이때, 무조건 최대치를 팔 필요는 없으므로, 현재 높이 - 1 까지만 판다.
+ *		2-3. 매 경로 진입마다 최대치 갱신. 
+ *
+ */
 public class Solution {
 
 	private static int[] dy = { -1, 1, 0, 0 };
 	private static int[] dx = { 0, 0, -1, 1 };
 
-	private static boolean[][] visited;
+	private static byte[] visited;
 	private static int[][] map;
 	private static Pos[] maxPoses;
 
@@ -21,7 +33,7 @@ public class Solution {
 		mapSize = read(tokenizer);
 		digDepth = read(tokenizer);
 
-		visited = new boolean[mapSize][mapSize];
+		visited = new byte[mapSize];
 		map = new int[mapSize][mapSize];
 		maxPoses = new Pos[64];
 
@@ -65,17 +77,17 @@ public class Solution {
 			int nrow = cur.row + dy[dir];
 			int ncol = cur.col + dx[dir];
 
-			if (nrow >= 0 && nrow < map.length && ncol >= 0 && ncol < map.length && !visited[nrow][ncol]) {
+			if (nrow >= 0 && nrow < map.length && ncol >= 0 && ncol < map.length && (visited[nrow] & (1 << ncol)) == 0) {
 				if (cur.height > map[nrow][ncol]) {
-					visited[nrow][ncol] = true;
+					visited[nrow] |= (1 << ncol);
 					dfs(new Pos(nrow, ncol, cur.isDigable, map[nrow][ncol], cur.length + 1));
-					visited[nrow][ncol] = false;
+					visited[nrow] &= ~(1 << ncol);
 				}
 
 				if (cur.isDigable && cur.height > map[nrow][ncol] - digDepth && cur.height <= map[nrow][ncol]) {
-					visited[nrow][ncol] = true;
+					visited[nrow] |= (1 << ncol);
 					dfs(new Pos(nrow, ncol, false, cur.height - 1, cur.length + 1));
-					visited[nrow][ncol] = false;
+					visited[nrow] &= ~(1 << ncol);
 				}
 			}
 		}
@@ -91,8 +103,9 @@ public class Solution {
 			init(tokenizer);
 
 			for (int idx = 0; idx < maxPosIdx; idx++) {
-				visited = new boolean[mapSize][mapSize];
-				visited[maxPoses[idx].row][maxPoses[idx].col] = true;
+				for (int row = 0; row < mapSize; row++)
+					visited[row] = 0;
+				visited[maxPoses[idx].row] |= (1 << maxPoses[idx].col);
 
 				dfs(maxPoses[idx]);
 			}
@@ -119,6 +132,11 @@ public class Solution {
 			this.isDigable = isDigable;
 			this.height = height;
 			this.length = length;
+		}
+
+		@Override
+		public String toString() {
+			return "Pos [row=" + row + ", col=" + col + ", height=" + height + ", length=" + length + ", isDigable=" + isDigable + "]";
 		}
 	}
 }
