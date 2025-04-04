ad33f3c05abdf7edc4b46aa2c20bd61b26d7f5b8
941kjw <jawonkoo@naver.com>
2025-04-04 13:37:38 +0900

    [D4] Title: [Professional] 키 순서, Time: 1,872 ms, Memory: 31,420 KB -BaekjoonHub

---
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -0,0 +1,89 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StreamTokenizer;
+import java.util.Arrays;
+
+public class Solution {
+
+	private static int studentNumber;
+	private static int compareCount;
+
+	private static boolean[][] students;
+	private static boolean[][] reversed;
+	private static boolean[] visited;
+
+	private static void init(StreamTokenizer tokenizer) throws IOException {
+		studentNumber = read(tokenizer);
+		compareCount = read(tokenizer);
+
+		students = new boolean[studentNumber + 1][studentNumber + 1];
+		reversed = new boolean[studentNumber + 1][studentNumber + 1];
+		visited = new boolean[studentNumber + 1];
+		for (int count = 0; count < compareCount; count++) {
+			int smaller = read(tokenizer);
+			int bigger = read(tokenizer);
+
+			students[smaller][bigger] = true;
+			reversed[bigger][smaller] = true;
+		}
+	}
+
+	private static int find() {
+		int counter = 0;
+		for (int cur = 1; cur <= studentNumber; cur++) {
+			Arrays.fill(visited, false);
+			int visitable = dfs(cur, 0, true);
+			Arrays.fill(visited, false);
+			int reversedVisitable = dfs(cur, 0, false);
+
+			
+			if ((visitable + reversedVisitable) == studentNumber + 1)
+				++counter;
+		}
+
+		return counter;
+	}
+
+	private static int dfs(int cur, int sum, boolean mode) {
+		visited[cur] = true;
+
+		int counter = 1;
+		for (int idx = 1; idx <= studentNumber; ++idx) {
+			if (visited[idx])
+				continue;
+
+			if (mode) {
+				if (students[cur][idx])
+					counter += dfs(idx, sum + 1, mode);
+			}
+			else {
+				if (reversed[cur][idx])
+					counter += dfs(idx, sum + 1, mode);
+			}
+
+		}
+
+		return counter;
+	}
+
+	private static int read(StreamTokenizer tokenizer) throws IOException {
+		tokenizer.nextToken();
+		return (int) tokenizer.nval;
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		StreamTokenizer tokenizer = new StreamTokenizer(reader);
+		StringBuilder builder = new StringBuilder();
+		int testCount = read(tokenizer);
+
+		for (int testNumber = 1; testNumber <= testCount; testNumber++) {
+			init(tokenizer);
+			builder.append('#').append(testNumber).append(' ').append(find()).append('\n');
+		}
+
+		System.out.println(builder);
+	}
+
+}
