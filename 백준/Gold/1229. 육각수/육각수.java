727b951d1f32a8ab59b1f2e8dd3bb4490eb9ad5e
941kjw <jawonkoo@naver.com>
2026-02-22 21:53:20 +0900

    [Gold IV] Title: 육각수, Time: 1228 ms, Memory: 17108 KB -BaekjoonHub

--- 백준/Gold/1229. 육각수/육각수.java
+++ 백준/Gold/1229. 육각수/육각수.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.StreamTokenizer;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 
 public class Main {
@@ -13,56 +15,64 @@ public class Main {
     private static BufferedWriter writer;
     private static StreamTokenizer tokenizer;
 
-
+    private static List<Integer> hex = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         initReader();
 
         int targetNumber = getNumber();
+
+        initHex(targetNumber);
         int result = findMinimum(targetNumber);
 
         System.out.println(result);
         clear();
     }
 
+    private static void initHex(int max) {
+        hex.add(0);
+        for(int k = 1;;++k){
+            int i = k * (2 * k - 1);
+            if(i > max)
+                break;
+            hex.add(i);
+        }
+    }
+
     private static int findMinimum(int targetNumber){
         int[] dp = new int[targetNumber + 1];
         Arrays.fill(dp,Integer.MAX_VALUE);
 
-        //
         int idx = 0;
-        int min;
-
         //1부터 목표 값까지 최소 개수를 계산해보기
-        for(int i = 1; i <= targetNumber; i++){            
-            //다음 육각수를 찾아서
-            int next = calcSixNumbers(idx + 1);
-            //딱 맞으면 1개만 더하면 됨.
-            if(i == next){
+        for (int i = 1; i <= targetNumber; i++) {
+            // idx를 올바르게 갱신
+            while (calcSixNumbers(idx + 1) <= i) idx++;
+
+            // 딱 육각수면 바로 1
+            if (calcSixNumbers(idx) == i) {
                 dp[i] = 1;
                 continue;
             }
-            //육각수가 더 작으면 다음 인덱스로 증가
-            if(i > next)
-                idx++;
-            
-            //위 -> 아래로 내려오면서, 최소 개수를 갱신.
-            for(int j = idx; j >= 0; --j){
-                min = calcSixNumbers(j);
-                if(dp[min] == Integer.MAX_VALUE)
-                    continue;
+
+            for (int j = idx; j >= 1; --j) {
+                int min = calcSixNumbers(j);
+                if (dp[min] == Integer.MAX_VALUE) continue;
+                if (dp[i - min] == Integer.MAX_VALUE) continue;
                 dp[i] = Math.min(dp[i - min] + dp[min], dp[i]);
             }
         }
 
         return dp[targetNumber];
     }
-    
+
     //육각수의 점 개수는 n(2n - 1)이다.
    private static int calcSixNumbers(int n){
         if(n <= 0)
             return 0;
-        return 2 * n * n - n;
+       if (n >= hex.size())
+           return Integer.MAX_VALUE;
+       return hex.get(n);
    }
 
     private static void clear() throws IOException {
